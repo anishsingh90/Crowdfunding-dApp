5380c603360afb9842350440483cb1147a661dd9
ANISH-SINGH <anishraaz90@gmail.com>
2025-03-12 18:12:07 +0530

    Frontend Setup

--- crowdfunding/Blockchain/hardhat.config.js
+++ crowdfunding/Blockchain/hardhat.config.js
@@ -6,9 +6,9 @@ module.exports = {
     networks: {
       hardhat: {},
       gorili: {
-        url: 'https://rpc.ankr.com/eth_goerli',
-        accounts: 
-      }
+        url: "https://rpc.ankr.com/eth_goerli",
+        accounts: [`0x${process.env.PRIVATE_KEY}`],
+      },
     },
     settings: {
       optimizer: {
